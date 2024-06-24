af7e9c4682206baa225ce68c05b148b1d23668ce
jiyun <ranji12@naver.com>
2024-06-24 23:58:16 +0900

    GET서버 연결 성공

--- serverTest.js
+++ serverTest.js
@@ -2,6 +2,19 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     if(req.method==='GET'){
-        
+        res.statusCode=200;
+        res.setHeader('Content-Type','text/html; charset=utf-8')
+        res.write('hi')
+        res.end()
+    }
+});
+
+let port = 8080;
+server.listen(port,(err)=>{
+    if(err){
+        console.error(err);
+    }
+    else{
+        console.log("localhost:8080")
     }
 });
